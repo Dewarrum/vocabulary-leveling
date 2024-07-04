1c1f3eaa0c9def005638095eb287f3f75e49c9a8
Yuri Tsai <dewarrum@gmail.com>
2024-07-04 16:55:27 +0900

    feat: add admin page to upload video and subtitles

---
+++ web/src/routes/admin/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { createProfileQuery } from '$lib/api/auth';
+	import Header from '$lib/components/Header.svelte';
+
+	const profileQuery = createProfileQuery();
+</script>
+
+<Header />
+{#if $profileQuery.isSuccess && $profileQuery.data && $profileQuery.data.roles.includes('Admin')}
+	<slot />
+{:else}
+	<div>You do not have access to this page</div>
+{/if}
---
+++ web/src/routes/admin/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts"></script>
+
+<main class="mx-auto flex w-3/4 flex-col items-center pt-8">
+	<form
+		class="flex flex-col gap-4"
+		action="/api/admin/videos/upload"
+		method="POST"
+		enctype="multipart/form-data"
+	>
+		<label for="videoFile">Select video</label>
+		<input id="videoFile" type="file" name="video" />
+
+		<label for="videoName">Video name</label>
+		<input
+			id="videoName"
+			type="text"
+			name="videoName"
+			class="rounded-md border-2 border-gray-300 px-4 py-2"
+		/>
+
+		<label for="subtitlesFile">Select subtitles</label>
+		<input id="subtitlesFile" type="file" name="subtitles" />
+
+		<button class="rounded-md border px-4 py-2">Upload</button>
+	</form>
+</main>
